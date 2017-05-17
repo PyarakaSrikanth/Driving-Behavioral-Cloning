de6c1731b80badf518d21cb023423d0d7adaae06
Farhan Ahmad <farhanhubble@gmail.com>
2017-05-17 12:06:27 +0530

    Change speed to 30MPH from 9MPH.

--- drive.py
+++ drive.py
@@ -27,6 +27,7 @@ class SimplePIController:
         self.Kp = Kp
         self.Ki = Ki
         self.set_point = 0.
+    
         self.error = 0.
         self.integral = 0.
 
@@ -44,7 +45,7 @@ class SimplePIController:
 
 
 controller = SimplePIController(0.1, 0.002)
-set_speed = 9
+set_speed = 30
 controller.set_desired(set_speed)
 
 
